474404aafdf718fdfdbd3e6c05914c94e6974879
Alfarizzi27 <farizyoga04.gmail.com>
2024-03-11 11:50:00 +0700

    add moduleexports

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-function tambah(a, b) {
-    return a + b
-}
+const { tambah, kurang, kali, bagi } = require('./calculate')
 
-function kurang(a, b) {
-    return a - b
+module.exports = {
+    tambah,
+    kurang,
+    kali,
+    bagi
 }
-
-function kali(a, b) {
-    return a * b
-}
-
-function bagi(a, b) {
-    return a / b
-}
-
---
+++ test.js
@@ -0,0 +1,5 @@
+const calculate = require('./index')
+
+const plus = calculate.tambah(1, 4)
+
+console.log(plus);
