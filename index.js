43e450a7b61e0ca81b72be718818edf3e1a41d3c
Alfarizzi27 <farizyoga04.gmail.com>
2024-03-11 11:05:32 +0700

    Add funcstion calculate

---
+++ index.js
@@ -0,0 +1,18 @@
+function tambah(a, b) {
+    return a + b
+}
+
+function kurang(a, b) {
+    return a - b
+}
+
+function kali(a, b) {
+    return a * b
+}
+
+function bagi(a, b) {
+    return a / b
+}
+
+
+console.log(tambah(10, 4));
